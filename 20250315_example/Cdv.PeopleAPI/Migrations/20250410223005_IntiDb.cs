8cdb4efbe3db6fe17bf7c4c710b7d85bc07e4fda
JakubRobak01 <robak.jakub2001@gmail.com>
2025-04-11 01:01:41 +0200

    Projekt

---
+++ 20250315_example/Cdv.PeopleAPI/Migrations/20250410223005_IntiDb.cs
@@ -0,0 +1,42 @@
+﻿using Cdv.PeopleApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cdv.PeopleAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntiDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    
+        public DbSet<PersonEntity> People {get; set; }
+
+    }
+
+}
