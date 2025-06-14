6885208376c1f07ca64a64dd08ab8af188dfc298
JakubRobak01 <158343452+JakubRobak01@users.noreply.github.com>
2025-06-14 03:59:06 +0200

    Delete ProjektZaliczenieRobak28357/Models directory

--- ProjektZaliczenieRobak28357/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace AzureChat.Models;
-
-public class User
-{
-    public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public ICollection<Message>? Messages { get; set; }
-}
